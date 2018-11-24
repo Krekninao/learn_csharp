02e187c9770c2b63787b6676a587df09d0942642
Kreknin Sasha <kreknin98@mail.ru>
2018-11-24 11:57:16 +0700

    добавил ещё один конструктор

--- Shoping/Device.cs
+++ Shoping/Device.cs
@@ -7,18 +7,32 @@ namespace Shopping
     class Device: IDevice
     {
        
-        public Device(string Data)
+        public Device(string data)
         {
-            var dataarray = Data.Split(' ');
-            Name = dataarray[0];
-            Price = float.Parse(dataarray[1]);
-            Rating = int.Parse(dataarray[2]);
-            ProducingCountry = dataarray[3];
-            Id = int.Parse(dataarray[4]);
+            var dataArray = data.Split(' ');
+            Id = int.Parse(dataArray[0]);
+            Name = dataArray[1];
+            Price = float.Parse(dataArray[2]);
+            Rating = int.Parse(dataArray[3]);
+            ProducingCountry = dataArray[4];
+        }
+
+        public Device(int id, string name, double price, int rating, string producingCountry)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Rating = rating;
+            ProducingCountry = producingCountry;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} стоит {Price.ToString("F1")}$. Произведено в {ProducingCountry}";
         }
 
         public string Name { get; set; }
-        public float Price { get; set; }
+        public double Price { get; set; }
         public int Rating { get; set; }
         public int Id { get; set; }
         public string ProducingCountry { get; set; }
