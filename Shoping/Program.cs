a6c673555c39113b5cc5455025bbbd3f000b21b5
Kreknin Sasha <kreknin98@mail.ru>
2018-11-24 11:59:38 +0700

    Накидал логику.

--- Shoping/Program.cs
+++ Shoping/Program.cs
@@ -9,8 +9,19 @@ namespace Shopping
         {
             var bookDataFile = File.ReadAllLines("Books.txt");
             var bookShop = new BookShop(bookDataFile);
-
             var deviceDataFile = File.ReadAllLines("Devices.txt");
+            var deviceShop = new DeviceShop(deviceDataFile);
+            deviceShop.ShowItems();
+            deviceShop.BuyItem(1);
+            var coffeeMaker = new Device(5, "CoffeeMaker", 45.5f, 31, "Bangladesh");
+            deviceShop.AddItem(coffeeMaker);
+            
+            Console.WriteLine("Список стран-производителей:");
+            foreach (var country in deviceShop.ShowProductCountry())
+            {
+                Console.WriteLine(country);
+            }
+            Console.ReadKey();
         }
     }
 }
