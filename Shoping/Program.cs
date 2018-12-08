d2bd2f51fd2bb16a57c7c255b2d8ec424d1668da
Kreknin Sasha <kreknin98@mail.ru>
2018-12-08 13:55:57 +0700

    Удалил ненужое.

--- Shoping/Program.cs
+++ Shoping/Program.cs
@@ -11,10 +11,6 @@ namespace Shopping
             var bookShop = new BookShop(bookDataFile);
             var deviceDataFile = File.ReadAllLines("Devices.txt");
             var deviceShop = new DeviceShop(deviceDataFile);
-            deviceShop.ShowItems();
-            deviceShop.BuyItem(1);
-            var coffeeMaker = new Device(5, "CoffeeMaker", 45.5f, 31, "Bangladesh");
-            deviceShop.AddItem(coffeeMaker);
             
             Console.WriteLine("Список стран-производителей:");
             foreach (var country in deviceShop.ShowProductCountry())
