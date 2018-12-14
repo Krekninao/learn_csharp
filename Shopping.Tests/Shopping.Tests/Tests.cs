f2a491e22fdf5afeb00517bbbb5356d9455833a5
Kreknin Sasha <kreknin98@mail.ru>
2018-12-14 22:19:18 +0700

    Unit-Тесты на покупку и добавление книг и устройств+ маппинг.

--- Shopping.Tests/Shopping.Tests/Tests.cs
+++ Shopping.Tests/Shopping.Tests/Tests.cs
@@ -24,6 +24,11 @@ namespace Shopping.Tests
         {
             Assert.AreEqual(1, _bookShop.GetNumberOfItems());
         }
+        [Test]
+        public void CheckNumberOfDevices()
+        {
+            Assert.AreEqual(1, _deviceShop.GetNumberOfItems());
+        }
 
         [Test]
         public void CheckBuyingBook()
@@ -35,7 +40,35 @@ namespace Shopping.Tests
             Assert.AreEqual(30, book.Rating);
             Assert.AreEqual("King", book.Author);
         }
-
+        [Test]
+        public void CheckBuyingDevice()
+        {
+            var device = _deviceShop.BuyItem(1);
+            Assert.AreEqual(0, _deviceShop.GetNumberOfItems());
+            Assert.AreEqual("WashingMachine", device.Name);
+            Assert.AreEqual(75, device.Price);
+            Assert.AreEqual(13, device.Rating);
+            Assert.AreEqual("China", device.ProducingCountry);
+        }
+        [Test]
+        public void CheckAddingBook()
+        {
+            var newBook = new Book
+            {
+                Id = 2,
+                Name = "In a beautiful and furious world",
+                Author = "Platonov",
+                Price = 26.7,
+                Rating = 68
+            };
+            _bookShop.AddItem(newBook);
+            Assert.AreEqual(2, _bookShop.GetNumberOfItems());
+            var receivedBook = _bookShop.BuyItem(newBook.Id);
+            Assert.AreEqual("In a beautiful and furious world", receivedBook.Name);
+            Assert.AreEqual(26.7, receivedBook.Price);
+            Assert.AreEqual(68, receivedBook.Rating);
+            Assert.AreEqual("Platonov", receivedBook.Author);
+        }
         [Test]
         public void CheckAddingDevice()
         {
@@ -50,19 +83,11 @@ namespace Shopping.Tests
             _deviceShop.AddItem(newDevice);
             Assert.AreEqual(2, _deviceShop.GetNumberOfItems());
             var receivedDevice = _deviceShop.BuyItem(newDevice.Id);
-
-
+            Assert.AreEqual("GasCooker", receivedDevice.Name);
+            Assert.AreEqual(38.6, receivedDevice.Price);
+            Assert.AreEqual(49, receivedDevice.Rating);
+            Assert.AreEqual("Italy", receivedDevice.ProducingCountry);
         }
 
-        [Test]
-        public void CheckBuyingDevice()
-        {
-            var device = _deviceShop.BuyItem(1);
-            Assert.AreEqual(0, _deviceShop.GetNumberOfItems());
-            Assert.AreEqual("WashingMachine", device.Name);
-            Assert.AreEqual(75, device.Price);
-            Assert.AreEqual(13, device.Rating);
-            Assert.AreEqual("China", device.ProducingCountry);
-        }
     }
 }
